d6c351baf26b8cbc96ebc83b440b860ad70589b7
Julien Grangé-Guermente <julien.grangeguermente@gmail.com>
2021-07-01 17:47:36 +0200

    Updated interactive pricing component

--- interactive-pricing/js/script.js
+++ interactive-pricing/js/script.js
@@ -9,17 +9,32 @@ window.onload = function() {
     const pageviewsDisplay = document.getElementsByClassName('main-body__title--pageviews')[0];
     // Get the price span
     const priceDisplay = document.getElementsByClassName('main-body__title--price')[0];
+    // Get the checkbox value    
+    const billingCheckbox = document.querySelector('#billing-checkbox'); 
+    // Stores final price
+    let finalPrice = 16;
+
 
     sliderValue.addEventListener('input', function(sliderChoice) {
         pricingValue = sliderChoice.target.value;
-        let billingCheckbox = document.querySelector('#billing-checkbox:checked'); // Get the checkbox value
         // Check if the checkbox is checked (a lot of "check")
-        if (billingCheckbox != null) {
-            pageviewsDisplay.innerText = pricing[pricingValue][0];
-            priceDisplay.innerText = "$" + pricing[pricingValue][1]*0.75 + ".00"; // Apply the 25% discount
-        } else if (billingCheckbox == null) {
-            pageviewsDisplay.innerText = pricing[pricingValue][0];
-            priceDisplay.innerText = "$" + pricing[pricingValue][1] + ".00";
+        if (billingCheckbox.checked) {
+            finalPrice =  pricing[pricingValue][1]*0.75;
+        } else {
+            finalPrice = pricing[pricingValue][1];
         }
+        pageviewsDisplay.innerText = pricing[pricingValue][0];
+        priceDisplay.innerText = "$" + finalPrice + ".00";
     });
+
+    const setDiscount = () => {
+        if (!billingCheckbox.checked) {
+            priceDisplay.innerHTML = `$${finalPrice}.00`
+        }
+        else {
+            priceDisplay.innerHTML = `$${finalPrice * 0.75}.00`;
+        }
+    };
+
+    billingCheckbox.addEventListener('input', setDiscount);
 }
